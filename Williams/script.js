231ece653da8e9af8aeb29b6e214047d0f1a0ded
Williams <valerow@outlook.com>
2023-01-04 07:49:49 -0500

    Portafolio

---
+++ Williams/script.js
@@ -0,0 +1,16 @@
+// const toggleBox = document.querySelector('.toogleBox');
+const navigation = document.querySelector('.navigation');
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
+
+// function showMenu() {
+//   navigation.classList.toggle('active');
+//   menu.classList.toggle('hide');
+//   closed.classList.toggle('show');
+// }
+
+function showMenu() {
+  navigation.classList.toggle('active');
+  menu.classList.toggle('hide');
+  close.classList.toggle('show');
+}
